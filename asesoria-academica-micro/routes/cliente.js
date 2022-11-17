cf7ffe5be37b275703ee6dcf580877a22f354ed6
MariaUsuga <105682206+MariaUsuga@users.noreply.github.com>
2022-11-17 16:27:24 -0500

    microservicio-clientes

---
+++ asesoria-academica-micro/routes/cliente.js
@@ -0,0 +1,38 @@
+const { Router } = require('express');
+const { validationResult, check } = require('express-validator');
+const router = Router();
+const Cliente = require('../models/Cliente');
+
+router.get('/', async function(req, res) {
+    try {
+        const clientes = await Cliente.find();
+        console.log(clientes);
+        res.send(clientes);
+    } catch(error) {
+        console.log(error);
+        res.send('Ocurrio un error');
+    }
+});
+
+router.get('/:clienteId', async function(req, res) {
+    try {
+        let clientes = await Cliente.findById(req.params.clienteId);
+        if (!clientes) {
+            return res.status(400).send('El cliente no existe');
+        }
+
+        const validaciones = validationResult(req);
+        if (validaciones.length > 0) {
+            return res.status(400).send(validaciones);
+        }
+
+
+        res.send(clientes);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un error al consultar el cliente');
+    }
+});
+
+
+module.exports = router;
